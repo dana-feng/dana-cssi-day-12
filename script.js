97f9aba4b5a14b2e7ceda64b192749e704cccd4b
Glitch (p5-js-template-cssi-2020) <none>
2020-07-19 20:28:27 +0000

    ⛩🎾 Checkpoint ./index.html:15945984/9 ./script.js:15945984/7376

--- script.js
+++ script.js
@@ -1,19 +1,96 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions. 
-/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+// Add to this list as you consult the p5.js documentation for other functions.
+/* global createCanvas, colorMode, HSB, width, height, random, background, fill, noFill, color, random,
+          rect, ellipse, stroke, image, loadImage, frameRate, collideCircleCircle, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
           keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-let backgroundColor;
+let backgroundColor, playerSnake, currentApple, score;
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
   backgroundColor = 95;
+  frameRate(12);
+  playerSnake = new Snake();
+  currentApple = new Apple();
+  score = 0;
 }
 
 function draw() {
   background(backgroundColor);
+  // The snake performs the following four methods:
+  playerSnake.moveSelf();
+  playerSnake.showSelf();
+  playerSnake.checkCollisions();
+  playerSnake.checkApples();
+  // The apple needs fewer methods to show up on screen.
+  currentApple.showSelf();
+  // We put the score in its own function for readability.
+  displayScore();
 }
+
+function displayScore() {}
+
+class Snake {
+  constructor() {
+    this.size = 10;
+    this.x = width / 2;
+    this.y = height - 10;
+    this.direction = "N";
+    this.speed = 12;
+  }
+
+  moveSelf() {
+    if (this.direction === "N") {
+      this.y -= this.speed;
+    } else if (this.direction === "S") {
+      this.y += this.speed;
+    } else if (this.direction === "E") {
+      this.x += this.speed;
+    } else if (this.direction === "W") {
+      this.x -= this.speed;
+    } else {
+      console.log("Error: invalid direction");
+    }
+  }
+
+  showSelf() {
+    stroke(240, 100, 100);
+    noFill();
+    rect(this.x, this.y, this.size, this.size);
+    noStroke();
+  }
+
+  checkApples() {}
+
+  checkCollisions() {}
+
+  extendTail() {}
+}
+
+class Apple {
+  constructor() {}
+
+  showSelf() {}
+}
+
+function keyPressed() {
+  console.log("key pressed: ", keyCode);
+  if (keyCode === UP_ARROW && playerSnake.direction != "S") {
+    playerSnake.direction = "N";
+  } else if (keyCode === DOWN_ARROW && playerSnake.direction != "N") {
+    playerSnake.direction = "S";
+  } else if (keyCode === RIGHT_ARROW && playerSnake.direction != "W") {
+    playerSnake.direction = "E";
+  } else if (keyCode === LEFT_ARROW && playerSnake.direction != "E") {
+    playerSnake.direction = "W";
+  } else {
+    console.log("wrong key");
+  }
+}
+
+function restartGame() {}
+
+function gameOver() {}
